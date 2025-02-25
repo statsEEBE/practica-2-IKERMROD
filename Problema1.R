1ef7448edf71eed60a85cf2529d3b19bf7cf173f
iker.munoz.rodriguez <iker.munoz.rodriguez@auleseebe.upc.edu>
2025-02-25 09:45:12 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,9 @@
 #Codigo para problema 1
+#coeficiente de determinaciones multiple R-squared
+Rsq <- 0.1923
+cor <- sqrt(Rsq) #correlacion
+
+m <- 0.06576 #mirando los datos, ahi esta en los beneficios en x
+b <- 11.7957 #igual que m, está en los datos del enunciado
+
+prediccion_apartado_d <- m*647.1+b
