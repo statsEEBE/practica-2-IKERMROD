fbd88cea76a47d18949a9e9de7afc279ece6c4cb
iker.munoz.rodriguez <iker.munoz.rodriguez@auleseebe.upc.edu>
2025-02-25 09:54:06 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -7,3 +7,14 @@ m <- 0.06576 #mirando los datos, ahi esta en los beneficios en x
 b <- 11.7957 #igual que m, está en los datos del enunciado
 
 prediccion_apartado_d <- m*647.1+b
+
+
+## CARGAR DATOS DE FICHEROS DE TEXTO
+data <- read.delim("air.txt", dec=",", sep="\t")
+data
+
+data <- read.table("air.txt", dec=",", header=TRUE, sep=" ")
+data
+head(data)
+
+data$Wind
